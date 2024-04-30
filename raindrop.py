c7c48e41a72b39b3298011612cfe1e29fda6e3a6
epfoshe <fosheevelyn@gmail.com>
2024-04-29 21:53:23 -0500

    Added class(Object) to thunder.py and outline

---
+++ raindrop.py
@@ -0,0 +1,17 @@
+import pygame
+import pygame.sprite import Sprite
+import random
+
+class Raindrop(Sprite):
+    """Create Sprite of Raindrop"""
+    def __init__(self, rain):
+        """Initialize attributes"""
+        super().__init__()
+        self.screen = rain.screen
+        self.raindrop_speed = random.randrange(1,10)
+
+        """Load Image into VSC"""
+
+        """WIDTH AND HEIGHT"""
+    def update(self):
+        """Raindrop Sprite Moves Downward"""
---
+++ thunder.py
@@ -0,0 +1,30 @@
+import random
+import pygame
+
+from raindrop import Raindrop
+from PIL import Image 
+
+class Rain_action():
+    def __init__(self):
+        pygame.init
+
+        """Create Screen"""
+
+        """Set the caption"""
+
+    def Rain(self):
+        """Loop Rain Object"""
+
+    def music(self):
+        """Create Background Music"""
+
+    def _check_events(self):
+        """distinguish updates """
+
+
+
+def main():
+
+
+if __name__ == "__main__": 
+    main()
